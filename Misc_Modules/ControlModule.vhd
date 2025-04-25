7197b34d63eeeed4668c1dbd74052985ce4b43f9
taconoodle <giannisvlassis1@gmail.com>
2025-04-25 17:35:57 +0300

    Fixed a few bugs and cleaned up the control module's code

--- Misc_Modules/ControlModule.vhd
+++ Misc_Modules/ControlModule.vhd
@@ -54,7 +54,7 @@ entity ControlModule is
 end ControlModule;
 
 architecture Behavioral of ControlModule is
-	type cycle_type is (reset, fetch, branch, branchNE, dec, branch_dec, r_exec, i_exec, memread, memwrite, alu_writeback, mem_writeback);
+	type cycle_type is (fetch, branch, branchNE, dec, r_exec, i_exec, memread, memreadbyte, memwrite, alu_writeback, mem_writeback);
 	signal cycle_state : cycle_type;
 	
 	signal opcode : STD_LOGIC_VECTOR (5 downto 0);
@@ -107,39 +107,28 @@ begin
 			cycle_state <= fetch;
 		else
 			case cycle_state is
-				when reset =>
-					if instr_state = b_op or instr_state = beq_op or instr_state = bne_op or instr_state = sw_op then
-						cycle_state <= branch_dec;
-					else
-						cycle_state <= dec;
-					end if;
 				when fetch =>
-					if instr_state = b_op or instr_state = beq_op or instr_state = bne_op or instr_state = sw_op then
-						cycle_state <= branch_dec;
-					else
-						cycle_state <= dec;
-					end if;
-				when branch_dec =>
-					if instr_state = sw_op then
-						cycle_state <= i_exec;
-					elsif instr_state = bne_op then
-						cycle_state <= branchNE;
-					else
-						cycle_state <= branch;
-					end if;
+					cycle_state <= dec;
 				when dec =>
 					if instr_state = NOP then
 						cycle_state <= fetch;
-					elsif instr_state = li_op or instr_state = lui_op or instr_state = andi_op or instr_state = ori_op then
+					elsif instr_state = li_op or instr_state = lui_op or instr_state = addi_op or instr_state = andi_op or instr_state = ori_op or
+											  instr_state = sw_op or instr_state = lw_op or instr_state = lb_op then
 						cycle_state <= i_exec;
+					elsif instr_state = bne_op then
+						cycle_state <= branchNE;
+					elsif instr_state = b_op or instr_state = beq_op then
+						cycle_state <= branch;
 					else
 						cycle_state <= r_exec;
 					end if;
 				when r_exec =>
 					cycle_state <= alu_writeback;
 				when i_exec =>
-					if instr_state = lw_op or instr_state = lb_op then
+					if instr_state = lw_op then
 						cycle_state <= memread;
+					elsif instr_state = lb_op then
+						cycle_state <= memreadbyte;
 					elsif instr_state = sw_op then
 						cycle_state <= memwrite;
 					else
@@ -150,6 +139,8 @@ begin
 				when branchNE =>
 					cycle_state <= fetch;
 				when memread =>
+					cycle_state <= mem_writeback;
+				when memreadbyte =>
 					cycle_state <= mem_writeback;
 				when memwrite =>
 					cycle_state <= fetch;
@@ -174,7 +165,7 @@ begin
 	
 	RF_WrData_Sel <= '1' when (cycle_state = mem_writeback) else '0';
 	
-	RF_B_sel <= '1' when (cycle_state = branch_dec) else '0';
+	RF_B_sel <= '1' when (instr_state = beq_op) OR (instr_state = bne_op) OR (instr_state = b_op) OR (instr_state = sw_op) else '0';
 	
 	ALU_Bin_sel <= '1' when (cycle_state = i_exec) else '0';
 	
@@ -185,23 +176,24 @@ begin
 					
 	Mem_WrEn <= '1' when (cycle_state = memwrite) else '0';
 	
-	Byte_ExtrEn <= '1' when (instr_state = lb_op) else '0';
+	Byte_ExtrEn <= '1' when (cycle_state = memreadbyte) else '0';
 	
 	Branch_Eq <= '1' when (cycle_state = branch) else '0';
 	
 	Branch_not_Eq <= '1' when (cycle_state = branchNE) else '0';
 	
-	InstrRegEn <= '1' when (cycle_state = fetch) OR (cycle_state = reset) else '0';
+	InstrRegEn <= '1' when (cycle_state = fetch) else '0';
 	
-	RF_A_RegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
+	RF_A_RegEn <= '1' when (cycle_state = dec) else '0';
 	
-	RF_B_RegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
+	RF_B_RegEn <= '1' when (cycle_state = dec) else '0';
 	
-	ImmedRegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
+	ImmedRegEn <= '1' when (cycle_state = dec) else '0';
 	
 	ALU_RegEn <= '1' when (cycle_state = r_exec) OR (cycle_state = i_exec) else '0';
 	
-	PC_SelRegEn <= '1' when (cycle_state = branch) OR (cycle_state = branchNE) else '0';
+--	PC_SelRegEn <= '1' when (cycle_state = branch) OR (cycle_state = branchNE) else '0';
+	PC_SelRegEn <= '1';
 	
-	MemDataRegEn <= '1' when (cycle_state = memwrite) else '0';
+	MemDataRegEn <= '1' when (cycle_state = memread) OR (cycle_state = memreadbyte) else '0';
 end Behavioral;
