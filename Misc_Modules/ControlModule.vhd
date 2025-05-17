7af7a43d9e0d7115dd650ad49147aa900fba627d
taconoodle <giannisvlassis1@gmail.com>
2025-05-17 21:40:40 +0300

    Modified Control and the Top CPU module a bit to prepare for the full modification of Control

--- Misc_Modules/ControlModule.vhd
+++ Misc_Modules/ControlModule.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    22:14:36 04/19/2025 
--- Design Name: 
--- Module Name:    ControlModule - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -43,17 +24,11 @@ entity ControlModule is
 			  Mem_WrEn : out STD_LOGIC;
 			  Byte_ExtrEn : out STD_LOGIC;
 			  Branch_Eq : out STD_LOGIC;
-			  Branch_not_Eq : out STD_LOGIC;
-			  InstrRegEn : out STD_LOGIC;
-			  RF_A_RegEn : out STD_LOGIC;
-			  RF_B_RegEn : out STD_LOGIC;
-			  ImmedRegEn : out STD_LOGIC;
-			  ALU_RegEn : out STD_LOGIC;
-			  MemDataRegEn : out STD_LOGIC);
+			  Branch_not_Eq : out STD_LOGIC);
 end ControlModule;
 
 architecture Behavioral of ControlModule is
-	type cycle_type is (fetch, branch, branchNE, dec, r_exec, i_exec, memread, memreadbyte, memwrite, alu_writeback, mem_writeback);
+	type cycle_type is (fetch, branch, branchNE, dec, branch_dec, r_exec, i_exec, memread, memwrite, alu_writeback, mem_writeback);
 	signal cycle_state : cycle_type;
 	
 	signal opcode : STD_LOGIC_VECTOR (5 downto 0);
@@ -71,44 +46,31 @@ begin
 	begin
 		case opcode is
 			when "100000" =>
-				instr_state <= alu_op;	
-
+				instr_state <= alu_op;			
 			when "111000" =>
 				instr_state <= li_op;
-
 			when "111001" =>
 				instr_state <= lui_op;
-
 			when "110000" =>
 				instr_state <= addi_op;
-
 			when "110010" =>
 				instr_state <= andi_op;
-
 			when "110011" =>
 				instr_state <= ori_op;
-
 			when "111111" =>
 				instr_state <= b_op;
-
 			when "010000" =>
 				instr_state <= beq_op;
-				
 			when "010001" =>
 				instr_state <= bne_op;
-
 			when "000011" =>
 				instr_state <= lb_op;
-
 			when "001111" =>
 				instr_state <= lw_op;
-
 			when "011111" =>
 				instr_state <= sw_op;
-
 			when others =>
 				instr_state <= NOP;
-				
 		end case;
 	end process;
 
@@ -117,152 +79,88 @@ begin
 		wait until rising_edge(CLK);
 		if RST = '1' then
 			cycle_state <= fetch;
-			
-		else
-			case cycle_state is
-				when fetch =>
-					cycle_state <= dec;
-
-				when dec =>
-					if instr_state = NOP then
-						cycle_state <= alu_writeback;
-
-					elsif instr_state = li_op or 
-							instr_state = lui_op or 
-							instr_state = addi_op or 
-							instr_state = andi_op or 
-							instr_state = ori_op or
-							instr_state = sw_op or 
-							instr_state = lw_op or 
-							instr_state = lb_op then
-						cycle_state <= i_exec;
-
-					elsif instr_state = bne_op then
-						cycle_state <= branchNE;
-
-					elsif instr_state = b_op or 
-										instr_state = beq_op then
-						cycle_state <= branch;
-
-					else
-						cycle_state <= r_exec;
-					end if;
-					
-				when r_exec =>
-					cycle_state <= alu_writeback;
-
-				when i_exec =>
-					if instr_state = lw_op then
-						cycle_state <= memread;
-
-					elsif instr_state = lb_op then
-						cycle_state <= memreadbyte;
-
-					elsif instr_state = sw_op then
-						cycle_state <= memwrite;
-
-					else
-						cycle_state <= alu_writeback;
-						
-					end if;
-
-				when branch =>
-					cycle_state <= fetch;
-
-				when branchNE =>
-					cycle_state <= fetch;
-
-				when memread =>
-					cycle_state <= mem_writeback;
-
-				when memreadbyte =>
-					cycle_state <= mem_writeback;
-
-				when memwrite =>
-					cycle_state <= fetch;
-
-				when alu_writeback =>
-					cycle_state <= fetch;
-
-				when mem_writeback =>
-					cycle_state <= fetch;
-
-			end case;
-			
 		end if;
 		
-		
+		case cycle_state is
+			when fetch =>
+				cycle_state <= fetch when (instr_state = NOP) else
+									branch_dec when (instr_state = b_op) OR 
+														 (instr_state = beq_op) OR 
+ 														 (instr_state = bne_op) OR 
+														 (instr_state = sw_op) else
+									dec;
+			when branch_dec =>
+				cycle_state <= i_exec when (instr_state = sw_op) else
+									branchNE when (instr_state = bne) else
+									branch;
+			when dec =>
+				cycle_state <= i_exec when (instr_state = li_op) OR 
+													(instr_state = lui_op) OR 
+													(instr_state = andi_op) OR
+													(instr_state = ori_op) else
+									r_exec;
+			when r_exec =>
+				cycle_state <= alu_writeback;
+			when i_exec =>
+				cycle_state <= memread when (instr_state = lw_op) OR 
+													 (instr_state = lb_op) else
+									memwrite when (isntr_state = sw_op) else
+									alu_writeback;
+			when branch =>
+				cycle_state <= fetch;
+			when branchNE =>
+				cycle_state <= fetch;
+			when memread =>
+				cycle_state <= mem_writeback;
+			when memwrite =>
+				cycle_state <= fetch;
+			when alu_writeback =>
+				cycle_state <= fetch;
+			when mem_writeback =>
+				cycle_state <= fetch;
+		end case;
 	end process;
 	
-	PC_LdEn <= '1' when (cycle_state = memwrite) or 
-							  (cycle_state = branch) or 
-							  (cycle_state = branchNE) or 
-							  (cycle_state = alu_writeback) or 
-							  (cycle_state = mem_writeback) else 
-				  '0';
+	PC_LdEn <= '1' when (cycle_state = fetch) else '0';
 	
 	Immed_sel <= "11" when (instr_state = lui_op) else
-					 "10" when (instr_state = andi_op) OR 
-								  (instr_state = ori_op) else
-					 "01" when (instr_state = b_op) OR 
-								  (instr_state = beq_op) OR 
-								  (instr_state = bne_op) else
+					 "10" when (instr_state = andi_op) OR (instr_state = ori_op) else
+					 "01" when (instr_state = b_op) OR (instr_state = beq_op) OR (instr_state = bne_op) else
 					 "00";
 	
-	RF_WrEn <= '1' when (cycle_state = alu_writeback) OR 
-							  (cycle_state = mem_writeback) else 
-				  '0';
+	RF_WrEn <= '1' when (cycle_state = alu_writeback) OR (cycle_state = mem_writeback) else '0';
 	
-	RF_WrData_Sel <= '1' when (cycle_state = mem_writeback) else 
-					     '0';
+	RF_WrData_Sel <= '1' when (cycle_state = mem_writeback) else '0';
 	
-	RF_B_sel <= '1' when (instr_state = beq_op) OR 
-								(instr_state = bne_op) OR 
-								(instr_state = b_op) OR 
-								(instr_state = sw_op) else 
-				   '0';
+	RF_B_sel <= '1' when (cycle_state = branch_dec) else '0';
 	
-	ALU_Bin_sel <= '1' when (cycle_state = i_exec) else 
-				      '0';
+	ALU_Bin_sel <= '1' when (cycle_state = i_exec) else '0';
 	
-	ALU_func <= func(3 downto 0) when (instr_state = alu_op) else
-					opcode(3 downto 0) when (instr_state = addi_op) OR 
-													(instr_state = andi_op) OR 
-													(instr_state = ori_op) else
-					"0001" when (instr_state = beq_op) OR 
-								   (instr_state = bne_op) OR 
-									(instr_state = b_op) else
+	ALU_func <= func(3 downto 0) when (cycle_state = alu_op) else
+					opcode(3 downto 0) when (cycle_state = addi_op) OR (cycle_state = andi_op) OR (cycle_state = ori_op) else
+					"0001" when (cycle_state = beq_op) OR (cycle_state = bne_op) OR (cycle_state = b_op) else
 					"0000";
 					
-	Mem_WrEn <= '1' when (cycle_state = memwrite) else 
-					'0';
+	Mem_WrEn <= '1' when (cycle_state = memwrite) else '0';
 	
-	Byte_ExtrEn <= '1' when (cycle_state = memreadbyte) else 
-						'0';
+	Byte_ExtrEn <= '1' when (instr_state = lb_op) else '0';
 	
-	Branch_Eq <= '1' when (cycle_state = branch) else 
-					 '0';
+	Branch_Eq <= '1' when (cycle_state = branch) else '0';
 	
-	Branch_not_Eq <= '1' when (cycle_state = branchNE) else 
-						  '0';
+	Branch_not_Eq <= '1' when (cycle_state = branchNE) else '0';
 	
-	InstrRegEn <= '1' when (cycle_state = fetch) else 
-					  '0';
+	InstrRegEn <= '1' when (cycle_state = fetch) else '0';
 	
-	RF_A_RegEn <= '1' when (cycle_state = dec) else 
-					  '0';
+	RF_A_RegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
 	
-	RF_B_RegEn <= '1' when (cycle_state = dec) else 
-					  '0';
+	RF_B_RegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
 	
-	ImmedRegEn <= '1' when (cycle_state = dec) else 
-					  '0';
+	ImmedRegEn <= '1' when (cycle_state = dec) OR (cycle_state = branch_dec) else '0';
 	
-	ALU_RegEn <= '1' when (cycle_state = r_exec) OR 
-								 (cycle_state = i_exec) else 
-					 '0';
+	ALU_RegEn <= '1' when (cycle_state = r_exec) OR (cycle_state = i_exec) else '0';
 	
-	MemDataRegEn <= '1' when (cycle_state = memread) OR 
-									 (cycle_state = memreadbyte) else 
-						 '0';
+	PC_SelRegEn <= '1' when (cycle_state = branch) OR (cycle_state = branchNE) else '0';
+	
+	MemDataRegEn <= '1' when (cycle_state = memwrite) else '0';
 end Behavioral;
+
--- Misc_Modules/Single_CPU.vhd
+++ Misc_Modules/Single_CPU.vhd
@@ -51,13 +51,7 @@ architecture Behavioral of Single_CPU is
 			  Mem_WrEn : out STD_LOGIC;
 			  Byte_ExtrEn : out STD_LOGIC;
 			  Branch_Eq : out STD_LOGIC;
-			  Branch_not_Eq : out STD_LOGIC;
-			  InstrRegEn : out STD_LOGIC;
-			  RF_A_RegEn : out STD_LOGIC;
-			  RF_B_RegEn : out STD_LOGIC;
-			  ImmedRegEn : out STD_LOGIC;
-			  ALU_RegEn : out STD_LOGIC;
-			  MemDataRegEn : out STD_LOGIC);
+			  Branch_not_Eq : out STD_LOGIC);
 	end component;
 	
 	component DATAPATH is
@@ -68,64 +62,40 @@ architecture Behavioral of Single_CPU is
 			  RST : in STD_LOGIC;
 			  PC_LdEn : in STD_LOGIC;
 			  Immed_sel : in STD_LOGIC_VECTOR (1 downto 0);
-			  RF_WrEn : in STD_LOGIC;
-			  RF_WrData_sel : in STD_LOGIC;
 			  RF_B_sel : in STD_LOGIC;
-			  ALU_Bin_sel : in STD_LOGIC;
-			  ALU_func : in STD_LOGIC_VECTOR (3 downto 0);
-			  Mem_WrEn : in STD_LOGIC;
-			  Byte_ExtrEn : in STD_LOGIC;
 			  Branch_Eq : in STD_LOGIC;
 			  Branch_not_Eq : in STD_LOGIC;
-			  InstrRegEn : in STD_LOGIC;
-			  RF_A_RegEn : in STD_LOGIC;
-			  RF_B_RegEn : in STD_LOGIC;
-			  ImmedRegEn : in STD_LOGIC;
-			  ALU_RegEn : in STD_LOGIC;
-			  MemDataRegEn : in STD_LOGIC);
+			  EX_Control : in STD_LOGIC_VECTOR (31 downto 0);
+			  M_Control : in STD_LOGIC_VECTOR (31 downto 0);
+			  WB_Control : in STD_LOGIC_VECTOR (31 downto 0));
 	end component;
 	
 	signal instruction : STD_LOGIC_VECTOR (31 downto 0); -- The current instruction
 	signal pc_load_enable : STD_LOGIC; -- Whether the program advances or is stalled
 	signal immediate_sel : STD_LOGIC_VECTOR (1 downto 0); -- Selects the format of the Immediate for instructions
-	signal rf_write_enable : STD_LOGIC; -- Whether to write data to register or not
-	signal rf_write_data_sel : STD_LOGIC; -- Determines where to take data from, to write to register
 	signal rf_B_sel : STD_LOGIC; -- Determines the second register file input
-	signal alu_bin_sel : STD_LOGIC; -- Determines the second alu operand
-	signal alu_function : STD_LOGIC_VECTOR (3 downto 0); -- Selects the function the ALU will perform
-	signal mem_write_enable : STD_LOGIC; -- Enables writing in the RAM
-	signal byte_extractor_enable : STD_LOGIC; -- Enables the extraction of bytes or lw operation
 	signal branch_eq_sig : STD_LOGIC; -- Signal for the beq and b operations
 	signal branch_not_eq_sig : STD_LOGIC; -- Signal for the bne operation
-	signal InstrRegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the instruction
-	signal RF_A_RegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the output A of the DECSTAGE
-	signal RF_B_RegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the output B of the DECSTAGE
-	signal ImmedRegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the immediate from the DECSTAGE
-	signal ALU_RegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the output data of the ALU
-	--signal PC_SelRegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the value of PC_Sel
-	signal MemDataRegEn_sig : STD_LOGIC; -- Signal controlling the register that holds the output data of MEMSTAGE
 	
+	signal EX_Control : STD_LOGIC_VECTOR (31 downto 0);
+	signal M_Control : STD_LOGIC_VECTOR (31 downto 0);
+	signal WB_Control : STD_LOGIC_VECTOR (31 downto 0);
 begin
 	control : ControlModule port map(CLK => CLK,
 												RST => RST, 
 												Instr => instruction, 
 												PC_LdEn => pc_load_enable, 
 												Immed_sel => immediate_sel,
-												RF_WrEn => rf_write_enable, 
-												RF_WrData_sel => rf_write_data_sel, 
+												RF_WrEn => WB_Control(1), 
+												RF_WrData_sel => WB_Control(0), 
 												RF_B_sel => rf_B_sel,
-												ALU_Bin_sel => alu_bin_sel, 
-												ALU_func => alu_function, 
-												Mem_WrEn => mem_write_enable,
-												Byte_ExtrEn => byte_extractor_enable, 
+												ALU_Bin_sel => EX_Control(0),
+												ALU_func => EX_Control(4 downto 1),
+												Mem_WrEn => M_Control(0),
+												Byte_ExtrEn => M_Control(1), 
 												Branch_Eq => branch_eq_sig, 
-												Branch_not_Eq => branch_not_eq_sig,
-												InstrRegEn => InstrRegEn_sig, 
-												RF_A_RegEn => RF_A_RegEn_sig, 
-												RF_B_RegEn => RF_B_RegEn_sig,
-												ImmedRegEn => ImmedRegEn_sig, 
-												ALU_RegEn => ALU_RegEn_sig,
-												MemDataRegEn => MemDataRegEn_sig);
+												Branch_not_Eq => branch_not_eq_sig
+												);
 	
 	path : DATAPATH port map(Instr => instruction, 
 									 Ovf => Ovf, 
@@ -134,20 +104,12 @@ begin
 									 RST => RST, 
 									 PC_LdEn => pc_load_enable, 
 									 Immed_sel => immediate_sel, 
-									 RF_WrEn => rf_write_enable,
-									 RF_WrData_sel => rf_write_data_sel, 
 									 RF_B_sel => rf_B_sel, 
-									 ALU_Bin_sel => alu_bin_sel, 
-									 ALU_func => alu_function, 
-									 Mem_WrEn => mem_write_enable,
-									 Byte_ExtrEn => byte_extractor_enable, 
 									 Branch_eq => branch_eq_sig, 
 									 Branch_not_Eq => branch_not_eq_sig,
-									 InstrRegEn => InstrRegEn_sig, 
-									 RF_A_RegEn => RF_A_RegEn_sig, 
-									 RF_B_RegEn => RF_B_RegEn_sig,
-									 ImmedRegEn => ImmedRegEn_sig, 
-									 ALU_RegEn => ALU_RegEn_sig,
-									 MemDataRegEn => MemDataRegEn_sig);
+									 EX_Control => EX_Control,
+									 M_Control => M_Control,
+									 WB_Control => WB_Control
+									 );
 end Behavioral;
 
